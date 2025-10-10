658e9918574778301f37c0eddb1845afb4ef21c4
Horizon489732 <tranminhhien27102003@gmail.com>
2025-10-10 12:56:06 -0400

    small fix

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,8 +1,17 @@
-import { createServer } from "./server.ts";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import morgan from "morgan";
 
-const port = process.env.PORT || 3001;
-const server = createServer();
+const app = express();
 
-server.listen(port, () => {
-  console.log(`api running on ${port}`);
+app.use(cors());
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("API is running");
 });
+
+app.listen(4000, () => console.log("Server running on port 4000"));
