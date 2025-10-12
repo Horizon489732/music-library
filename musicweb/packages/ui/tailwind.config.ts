cc5f39627d2eb36d7ddd50ba21362f9d89b1df96
Horizon489732 <tranminhhien27102003@gmail.com>
2025-10-12 01:48:38 -0400

    configure Shadcn

---
+++ musicweb/packages/ui/tailwind.config.ts
@@ -0,0 +1,16 @@
+import { type Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: "class", 
+  content: [
+    "../../apps/web/**/*.{ts,tsx}",
+    "../../apps/admin/**/*.{ts,tsx}",
+    "./src/components/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
